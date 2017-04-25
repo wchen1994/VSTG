3ef0257b24c7628507bc08433a6915ca3ff41779
wchen1994 <wchen1994@github.com>
2017-04-26 07:28:14 +1000

    Added class Board

--- VSTG/VSTG/Board.cc
+++ VSTG/VSTG/Board.cc
@@ -1,50 +1,80 @@
 #include "Board.hpp"
-/*
-Board::Board(int wndWidth, int wndHeight, int gridsize){
-	this->gridsize = gridsize;
-	width = (wndWidth + gridsize - 1) / gridsize;
-	height = (wndHeight + gridsize - 1) / gridsize;
+#include <stdlib.h>
 
-	grid.resize(height);
-	for (int i=0; i<height; i++){
-		grid[i].resize(width);
+Board::Tile::Tile(float & width_in, float & height_in)
+{
+	width = width_in;
+	height = height_in;
+}
+
+std::set<std::shared_ptr<GameObject>> Board::Tile::GetLayer() const
+{
+	return layerObject;
+}
+
+void Board::Tile::RemoveObject(std::shared_ptr<GameObject> pObject)
+{
+	layerObject.erase(pObject);
+}
+
+void Board::Tile::AddObject(std::shared_ptr<GameObject> pObject)
+{
+	layerObject.insert(pObject);
+}
+
+
+Board::Board(float & boardWidth_in, float & boardHeight_in, float & tileWidth_in, float & tileHeight_in)
+{
+	boardWidth = boardWidth_in;
+	boardHeight = boardHeight_in;
+	tileWidth = tileWidth_in;
+	tileHeight = tileHeight_in;
+	nCol = int(boardWidth / tileWidth + 1);
+	nRow = int(boardHeight / tileHeight + 1);
+	for (int i = 0; i < nCol*nRow; i++) {
+		tiles.push_back(std::make_shared<Tile>(Tile(tileWidth, tileHeight)));
 	}
 }
 
-void Board::insert(int row, int col, GameObject *pObject){
-	grid[row][col].insert(pObject);
-}
-
-void Board::remove(int row, int col, GameObject *pObject){
-	grid[row][col].erase(pObject);
-}
-
-void Board::processCollision(){
-	std::set<GameObject*> setObjs;
-	std::set<GameObject*>::iterator itPrimary;
-	std::set<GameObject*>::iterator itSecondary;
-
-	for (int row = 0; row < height; row++){
-		for (int col = 0; col < width; col++){
-			setObjs = grid[row][col];
-			for (itPrimary = setObjs.begin(); 
-			 	 itPrimary != setObjs.end(); 
-			 	 itPrimary++){
-				for (itSecondary = itPrimary; 
-					 itSecondary != setObjs.end(); 
-					 itSecondary++){
-					(*itPrimary)->OnCollisionEnter(*itSecondary);
-				}
+void Board::UpdateBoard()
+{
+	for (int i = 0; i < nCol*nRow; i++) {
+		std::set<std::shared_ptr<GameObject>> &layer = tiles[i]->GetLayer();
+		for (auto it_pObject = layer.begin(); it_pObject != layer.end(); it_pObject++) {
+			sf::Vector2f &pos = (*it_pObject)->getPosition();
+			const int x_new = int(pos.x / tileWidth);
+			const int y_new = int(pos.y / tileHeight);
+			if (x_new != i % nCol && y_new != i / nCol) {
+				tiles[i]->RemoveObject(*it_pObject);
+				tiles[y_new * nCol + x_new]->AddObject(*it_pObject);
 			}
 		}
 	}
 }
 
-void Board::clear(){
-	for (int row = 0; row < height; row++){
-		for (int col = 0; col < width; col++){
-			grid[row][col].clear();
+std::set<std::shared_ptr<Board::Tile>> Board::GetPotentialTile(int id_x, int id_y) const
+{
+	std::set<std::shared_ptr<Board::Tile>> sTile;
+	for (int y = std::max(0, id_y - 1); y <= std::min(nRow, id_y + 1); y++) {
+		for (int x = std::max(0, id_x - 1); x <= std::min(nCol, id_x + 1); x++) {
+			sTile.insert(tiles[y*nCol + x]);
 		}
 	}
+	return sTile;
 }
-*/
+
+void Board::RemoveObject(std::shared_ptr<GameObject> pObject)
+{
+	const sf::Vector2f pos = pObject->getPosition();
+	const int id_x = int(pos.x / tileWidth);
+	const int id_y = int(pos.y / tileHeight);
+	tiles[id_y * nCol + id_x]->RemoveObject(pObject);
+}
+
+void Board::AddObject(std::shared_ptr<GameObject> pObject)
+{
+	const sf::Vector2f pos = pObject->getPosition();
+	const int id_x = int(pos.x / tileWidth);
+	const int id_y = int(pos.y / tileHeight);
+	tiles[id_y * nCol + id_x]->AddObject(pObject);
+}
--- VSTG/VSTG/Board.hpp
+++ VSTG/VSTG/Board.hpp
@@ -3,24 +3,41 @@
 #include "GameObject.hpp"
 #include <set>
 #include <vector>
-/* Disable class Board not using
+
 class Board{
 private:
-	int gridsize;
-	int width;
-	int height;
-	std::vector< std::vector< std::set<GameObject*> > > grid;
+	class Tile {
+	public:
+		Tile(float& width_in, float& height_in);
+		std::set<std::shared_ptr<GameObject>> GetLayer() const;
+		void RemoveObject(std::shared_ptr<GameObject> pObject);
+		void AddObject(std::shared_ptr<GameObject> pObject);
+	private:
+		float width;
+		float height;
+		std::set<std::shared_ptr<GameObject>> layerObject;
+	};
 
 public:
-	Board(int wndWidth, int wndHeight, int gridsize);
-	int getGridSize(){ return gridsize; }
-	void setGridSize(int size){ gridsize = size; }
+	Board(float& boardWidth_in, float& boardHeight_in, float& tileWidth_in, float& tileHeight_in);
+	void UpdateBoard();
+	std::set<std::shared_ptr<Tile>> GetPotentialTile(int id_x, int id_y) const;
+	std::set<std::shared_ptr<Tile>> GetPotentialTile(float& pos_x, float& pos_y) const {
+		return GetPotentialTile(int(pos_x / tileWidth), int(pos_y / tileHeight));
+	}
+	std::set<std::shared_ptr<Tile>> GetPotentialTile(std::shared_ptr<GameObject>& pObject) const {
+		const sf::Vector2f pos = pObject->getPosition();
+		return GetPotentialTile(int(pos.x), int(pos.y));
+	}
+	void RemoveObject(std::shared_ptr<GameObject> pObject);
+	void AddObject(std::shared_ptr<GameObject> pObject);
 
-	void insert(int row, int col, GameObject *pObject);
-	void remove(int row, int col, GameObject *pObject);
-	void processCollision();
-	void clear();
+private:
+	float tileWidth;
+	float tileHeight;
+	float boardWidth;
+	float boardHeight;
+	int nRow;
+	int nCol;
+	std::vector<std::shared_ptr<Tile>> tiles;
 };
-
-static Board board(800, 600, 50);
-*/
