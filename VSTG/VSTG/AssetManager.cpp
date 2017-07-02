2dc2c6d6e1506d0ce43244356f91598287f6dbed
wchen1994 <wchen1994@github.com>
2017-07-02 16:13:04 +1000

    Added asset of memory block

--- VSTG/VSTG/AssetManager.cpp
+++ VSTG/VSTG/AssetManager.cpp
@@ -34,6 +34,28 @@ std::shared_ptr<sf::SoundBuffer> AssetManager::GetSoundBuffer(std::string const 
 	return nullptr;
 }
 
+std::shared_ptr<std::vector<char>> AssetManager::GetBlock(std::string const & path, size_t max_size)
+{
+	auto itBlock = m_pBlock.find(path);
+	if (itBlock == m_pBlock.end()) {
+		std::ifstream in_file;
+		in_file.open(path, std::ios::binary | std::ios::in);
+		in_file.seekg(0, in_file.end);
+		size_t size = in_file.tellg();
+		if (size != max_size)
+			return nullptr;
+		in_file.seekg(0, in_file.beg);
+		std::shared_ptr<std::vector<char>> pBlock = std::make_shared<std::vector<char>>(std::vector<char>(size));
+		in_file.read(pBlock->data(), size);
+		m_pBlock[path] = pBlock;
+		return pBlock;
+	}
+	else {
+		return itBlock->second;
+	}
+	return nullptr;
+}
+
 bool AssetManager::killLonePtr()
 {
 	for (auto it = m_pTexMap.begin(); it != m_pTexMap.end();) {
--- VSTG/VSTG/AssetManager.h
+++ VSTG/VSTG/AssetManager.h
@@ -5,6 +5,7 @@
 #include <SFML/Audio.hpp>
 #include <assert.h>
 #include <memory>
+#include <fstream>
 
 class AssetManager {
 public:
@@ -13,8 +14,10 @@ public:
 	AssetManager();
 	std::shared_ptr<sf::Texture> GetTexture(std::string const& path);
 	std::shared_ptr<sf::SoundBuffer> GetSoundBuffer(std::string const& path);
+	std::shared_ptr<std::vector<char>> GetBlock(std::string const& path, size_t max_size);
 	bool killLonePtr();
 private:
 	std::map<std::string, std::shared_ptr<sf::Texture>> m_pTexMap;
 	std::map<std::string, std::shared_ptr<sf::SoundBuffer>> m_pSoundMap;
+	std::map<std::string, std::shared_ptr<std::vector<char>>> m_pBlock;
 };
