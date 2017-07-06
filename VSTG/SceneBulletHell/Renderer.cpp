bbe4e020eecdf1be1fd89b2be591f0317bd44580
wchen1994 <wchen1994@github.com>
2017-07-06 22:57:46 +1000

    Implemeted Renderer class

--- VSTG/SceneBulletHell/Renderer.cpp
+++ VSTG/SceneBulletHell/Renderer.cpp
@@ -1,12 +1,46 @@
 #include "Renderer.h"
 
+// Static start
+std::set<std::shared_ptr<Renderer>> Renderer::setDrawObjects;
+// Static end
 
+void Renderer::draw(sf::RenderTarget & gfx)
+{
+	for (auto pRender : setDrawObjects) {
+		pRender->sprite.setPosition(pRender->position);
+		gfx.draw(pRender->sprite);
+	}
+}
 
-Renderer::Renderer()
+Renderer::Renderer(std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & origin, const sf::Vector2f & scale)
+{
+	if (pTex == nullptr)
+		pTex = CommResMeth::AssetManager::GetTexture("Resources/Textures/default.png");
+	this->pTex = pTex;
+	sprite.setTexture(*pTex);
+	sprite.setOrigin(origin);
+	sprite.setScale(scale);
+
+	setDrawObjects.insert(shared_from_this());
+}
+
+Renderer::Renderer(std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & scale) :
+	Renderer(pTex, { pTex->getSize().x / 2.f, pTex->getSize().y / 2.f }, scale)
 {
 }
 
+Renderer::Renderer(const sf::IntRect & rect, std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & origin, const sf::Vector2f & scale) :
+	Renderer(pTex, origin, scale)
+{
+	sprite.setTextureRect(rect);
+}
+
+Renderer::Renderer(const sf::IntRect & rect, std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & scale) :
+	Renderer(rect, pTex, {rect.width/2.f, rect.height/2.f}, scale)
+{
+}
 
 Renderer::~Renderer()
 {
+	setDrawObjects.erase(shared_from_this());
 }
--- VSTG/SceneBulletHell/Renderer.h
+++ VSTG/SceneBulletHell/Renderer.h
@@ -1,11 +1,28 @@
 #pragma once
 
 #include "Moveable.h"
+#include "AssetManager.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <set>
 
-class Renderer : public virtual Moveable
+#pragma comment(lib, "CommonResourcesMethods")
+
+class Renderer : public virtual Moveable, public std::enable_shared_from_this<Renderer>
 {
 public:
-	Renderer();
+	static std::set<std::shared_ptr<Renderer>> setDrawObjects;
+	static void draw(sf::RenderTarget &gfx);
+public:
+	Renderer(std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & origin, const sf::Vector2f & scale = sf::Vector2f(1.f, 1.f));
+	Renderer(std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & scale = sf::Vector2f(1.f, 1.f));
+	Renderer(const sf::IntRect & rect, std::shared_ptr<sf::Texture> pTex, const sf::Vector2f & origin,
+		const sf::Vector2f & scale = sf::Vector2f(1.f, 1.f));
+	Renderer(const sf::IntRect & rect, std::shared_ptr<sf::Texture> pTex,
+		const sf::Vector2f & scale = sf::Vector2f(1.f, 1.f));
 	~Renderer();
+private:
+	std::shared_ptr<sf::Texture> pTex;
+	sf::Sprite sprite;
 };
 
